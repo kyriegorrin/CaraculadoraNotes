abf2816810de05a8ca93771ade1851c4c604bc04
kyriegorrin <kyriegorrin@gmail.com>
2018-03-06 14:01:06 +0100

    Afegida autogeneració, debugada i presuntament llest per a començar a calcular assignatures

--- scriptNotes.js
+++ scriptNotes.js
@@ -1,8 +1,21 @@
+//Creació contenidors pertinents de notes
+var inputParcials = [];
+var inputLabos = [];
+var inputMisc = [];
+var inputFinals = [];
+
 //Aquesta funció bàsicament serà un switch criminalment llarg, però com que no tenim
 //API per als mètodes avaluatius i fer scrapping no és viable, és lo que hay. 
 //Probablement es podria tirar fent una BD per alguna banda amb els percentatges i 
 //càlculs emmagatzemats d'alguna manera, però pel moment es queda així.
 function submitAssignatura(){
+	//Abans que res, netejem el contingut dels divs i els inputs (per si s'ha executat el script abans)
+	clearDivsAndContainers();
+
+	//Posem el botó de calcular visible
+	document.getElementById("computeButton").removeAttribute("hidden");
+
+	//Agafem el nom de l'assignatura seleccionada
 	var nomAssig = document.getElementById("nom_assignatura").value;
 
 	//numAvaluacions és un array[0..3] que conté el numero d'actes avaluatius de cada tipus
@@ -14,12 +27,6 @@ function submitAssignatura(){
 	var numLabos = numAvaluacions[1];
 	var numMisc = numAvaluacions[2];
 	var numFinals = numAvaluacions[3];
-
-	//Creació contenidors pertinents
-	var inputParcials = [];
-	var inputLabos = [];
-	var inputMisc = [];
-	var inputFinals = [];
 
 	//Aconseguim referencies dels divs destinació
 	var divParcials = document.getElementById("div_parcials");
@@ -104,6 +111,49 @@ function submitAssignatura(){
 	document.getElementById("resultat").innerHTML = numLabos;
 }
 
-function computaNota(){
-	//TODO
+//Funció que neteja el contingut dels divs contenidors dels inputs i els
+//arrays globals que contenen les notes.
+function clearDivsAndContainers(){
+	var divParcials = document.getElementById("div_parcials");
+	var divLabos = document.getElementById("div_labos");
+	var divMisc = document.getElementById("div_misc");
+	var divFinals = document.getElementById("div_finals");
+
+	//L'array és per fer menys codi després quan apliquem el tractament a tots els divs
+	var arrayDivs = [divParcials, divLabos, divMisc, divFinals];
+
+	//Netejem tots els divs
+	for(i = 0; i < arrayDivs.length; i++){
+		while(arrayDivs[i].firstChild){
+			arrayDivs[i].removeChild(arrayDivs[i].firstChild);
+		}
+	}
+
+	//Fem clear de la nota calculada
+	document.getElementById("div_nota").innerHTML = "";
+
+	//Clear dels arrays
+	inputParcials = [];
+	inputLabos = [];
+	inputMisc = [];
+	inputFinals = [];
 }
+
+//La funció agafa els inputs dels vectors d'elements passats per paràmetre i computa la nota depenent de l'assignatura.
+//En realitat és un switch glorificat putament llarg.
+function computaNota(){
+	//Assignatura que tractem
+	var assignatura = document.getElementById("nom_assignatura").value;
+
+	//Nota calculada
+	var nota;
+
+	switch(assignatura){
+		//Algorismia
+		case "A":
+	}
+
+	//DEBUGGING DE VARIABLES GLOBALS
+	console.log(inputParcials.length);
+}
+
